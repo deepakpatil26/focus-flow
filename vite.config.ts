7a0c80d57b750b835de631bdea09e167d601f9cf
Deepak Patil <deepakpatil2612@gmail.com>
2025-05-29 10:33:14 +0530

    Update package.json and vite.config.ts for project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/focus-flow/', // Replace with your repo name
 });
